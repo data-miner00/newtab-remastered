229d18a4d5c3cff790084496eaaedeb459b84f71
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-13 00:48:25 +0800

    fix: Temporary remove layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
-  DefaultLayout
-    router-view/
+  //- DefaultLayout
+  router-view/
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import DefaultLayout from "./layouts/Default.vue";
+// import DefaultLayout from "./layouts/Default.vue";
 
 export default Vue.extend({
   components: {
-    DefaultLayout,
+    // DefaultLayout,
   },
 });
 </script>
