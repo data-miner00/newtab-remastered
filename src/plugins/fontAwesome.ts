29a9f4ca0d7956d8b76a3bcba3fda32439244475
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-12 22:26:28 +0800

    feat: Added more icons

--- src/plugins/fontAwesome.ts
+++ src/plugins/fontAwesome.ts
@@ -2,9 +2,16 @@ import Vue from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-import { faComment as farComment } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCoffee,
+  faDungeon,
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faComment as farComment,
+  faEnvelope as farEnvelope,
+} from "@fortawesome/free-regular-svg-icons";
 
-library.add(faCoffee, farComment);
+library.add(faCoffee, farComment, faDungeon, farEnvelope, faChevronDown);
 
 Vue.component("Fa", FontAwesomeIcon);
