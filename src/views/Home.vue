f81a5f67d81c03eb43a4a704757b7a3f5a52192a
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-13 00:48:57 +0800

    feat: Supercharged search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,25 @@
 <template lang="pug">
   .home
-    .searchbox
+    Header/
+    .greeting Good Morning
+    .searchbox(ref="hod")
       .service-selector(@click="toggleSelector")
         .service-icon(:title="service")
           img(:src="serviceLogo")
-        .dropdown-icon d
+        .dropdown-icon
+          fa(icon="caret-down")
         .hidden-selector(ref="hid")
           .icons(v-for="se in searchEngines" :key="se.id" @click="selectService(se.id)")
-            img(:src="se.logo")
+            div
+              img(:src="se.logo")
       .search-input
-        input(type="text" placeholder="Type here...")
+        input(type="text" v-model="searchQuery" :placeholder="searchPlaceholder" @focus="triggerDarkerBorder" @blur="triggerDarkerBorder" @keyup.enter="search")
 
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Header from "../components/Header.vue";
 
 type ISearchEngineService = {
   id: number;
@@ -24,7 +29,11 @@ type ISearchEngineService = {
 };
 
 export default Vue.extend({
+  components: {
+    Header,
+  },
   data: () => ({
+    searchQuery: "",
     serviceId: 1,
     service: "",
     serviceLogo: "",
@@ -35,7 +44,14 @@ export default Vue.extend({
   },
   methods: {
     search(): void {
-      //
+      if (!this.searchQuery) return;
+      if (this.searchQuery.startsWith("https://")) {
+        window.open(this.searchQuery, "_blank");
+      } else {
+        const parsedSearchText: string = this.searchQuery.replaceAll(" ", "+");
+        window.open(this.serviceQueryString + parsedSearchText, "_blank");
+      }
+      this.searchQuery = "";
     },
     selectService(_id: number): void {
       const { id, service, logo, queryString }: ISearchEngineService =
@@ -53,6 +69,14 @@ export default Vue.extend({
         selectorElement.style.display = "none";
       }
     },
+    triggerDarkerBorder() {
+      const searchElement: HTMLDivElement = this.$refs.hod as HTMLDivElement;
+      if (searchElement.classList.contains("focus")) {
+        searchElement.classList.remove("focus");
+      } else {
+        searchElement.classList.add("focus");
+      }
+    },
   },
   computed: {
     searchEngines(): ISearchEngineService[] {
@@ -61,57 +85,127 @@ export default Vue.extend({
           id: 1,
           service: "Google",
           logo: "/favicons/google.png",
-          queryString: "",
+          queryString: "https://www.google.com/search?q=",
         },
         {
           id: 2,
           service: "Duckduckgo",
           logo: "/favicons/duckduckgo.png",
-          queryString: "",
+          queryString: "https://www.duckduckgo.com/?q=",
         },
         {
           id: 3,
-          service: "Bing",
-          logo: "/favicons/bing.png",
-          queryString: "",
+          service: "Youtube",
+          logo: "/favicons/youtube.png",
+          queryString: "https://www.youtube.com/results?search_query=",
         },
         {
           id: 4,
+          service: "Bing",
+          logo: "/favicons/bing.png",
+          queryString: "https://www.bing.com/search?q=",
+        },
+        {
+          id: 5,
           service: "Yahoo",
           logo: "/favicons/yahoo.png",
-          queryString: "",
+          queryString: "https://search.yahoo.com/search?p=",
+        },
+        {
+          id: 6,
+          service: "Baidu",
+          logo: "/favicons/baidu.png",
+          queryString: "https://www.baidu.com/s?wd=",
+        },
+        {
+          id: 7,
+          service: "Yandex",
+          logo: "/favicons/yandex.png",
+          queryString: "https://www.yandex.com/search/?text=",
+        },
+        {
+          id: 8,
+          service: "Library Genesis",
+          logo: "/favicons/libgen.png",
+          queryString: "http://libgen.rs/scimag/?q=",
         },
       ];
+    },
+    searchPlaceholder(): string {
+      return `Search ${this.service} or type a URL`;
     },
   },
 });
 </script>
 
 <style lang="less" scoped>
+@import "../assets/less/_variables.less";
 .home {
-  height: 100%;
-  background: #eee;
+  height: 100vh;
 }
+
 .searchbox {
+  margin: 0 auto;
+  width: 600px;
   display: flex;
+  background: white;
+  border-radius: 5px;
+  border: 1px solid #eee;
+
+  &.focus {
+    border: 1px solid #ccc;
+  }
 
   .service-selector {
     display: flex;
-    padding: 20px;
-    background: #eee;
+    padding: 10px 30px;
     position: relative;
+    cursor: pointer;
+    border-right: 1px solid #eee;
+
+    .service-icon {
+      margin-right: 10px;
+    }
 
     .hidden-selector {
       position: absolute;
-      top: 100%;
+      top: calc(100% + 5px);
+      left: 0;
+      background: white;
+      border: 1px solid #eee;
+      border-radius: 5px;
+      padding: 10px 0;
+
+      .icons {
+        height: 48px;
+        width: 105px;
+        transition: background 0.2s;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        div {
+          width: fit-content;
+        }
+
+        &:hover {
+          background: #eee;
+        }
+      }
     }
   }
   .search-input {
-    padding: 20px;
+    flex-grow: 1;
+
     input {
+      font-size: 1rem;
+      color: #333;
+      padding: 10px 10px 10px 20px;
       outline: none;
       border: none;
       background: transparent;
+      width: 100%;
+      height: 100%;
     }
   }
 }
