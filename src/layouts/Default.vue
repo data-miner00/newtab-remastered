d105d3708b050b8e950e4d3a2c2525be159ecde8
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-12 22:26:08 +0800

    feat: Added default layout component

---
+++ src/layouts/Default.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .default-layout
+    Header/
+    slot/
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Header from "../components/Header.vue";
+
+export default Vue.extend({
+  components: {
+    Header,
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.default-layout {
+  min-height: 100vh;
+}
+</style>
