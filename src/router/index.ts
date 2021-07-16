8945385cd5545a9809157f49f438d5217864babb
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-16 14:57:29 +0800

    feat: Added routes for settings page

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,12 +7,12 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
   },
   {
     path: "/about",
-    name: "About",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -21,9 +21,47 @@ const routes: Array<RouteConfig> = [
   },
   {
     path: "/settings",
-    name: "Settings",
+    name: "settings",
     component: () =>
       import(/* webpackChunkName: "settings" */ "../views/Settings/index.vue"),
+    children: [
+      {
+        path: "",
+        name: "user",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../views/Settings/User.vue"),
+      },
+      {
+        path: "quicklinks",
+        name: "quicklinks",
+        component: () =>
+          import(
+            /* webpackChunkName: "quicklinks" */ "../views/Settings/Quicklinks.vue"
+          ),
+      },
+      {
+        path: "clock",
+        name: "clock",
+        component: () =>
+          import(/* webpackChunkName: "clock" */ "../views/Settings/Clock.vue"),
+      },
+      {
+        path: "background",
+        name: "background",
+        component: () =>
+          import(
+            /* webpackChunkName: "background" */ "../views/Settings/Background.vue"
+          ),
+      },
+      {
+        path: "server",
+        name: "server",
+        component: () =>
+          import(
+            /* webpackChunkName: "server" */ "../views/Settings/Server.vue"
+          ),
+      },
+    ],
   },
 ];
 
