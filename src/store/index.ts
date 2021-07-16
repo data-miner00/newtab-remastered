e2083258f4c4a6a90dce47b71f9a10f6878440c9
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-16 20:26:54 +0800

    feat: Added vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,34 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    username: null,
+    avatarUrl: null,
+    mailUrl: null,
+    newsUrl: null,
+    mapUrl: null,
+    bgUrl: null,
+  },
+  mutations: {
+    setUsername(state, payload) {
+      state.username = payload;
+    },
+    setAvatarUrl(state, payload) {
+      state.avatarUrl = payload;
+    },
+    setMailUrl(state, payload) {
+      state.mailUrl = payload;
+    },
+    setNewsUrl(state, payload) {
+      state.newsUrl = payload;
+    },
+    setMapUrl(state, payload) {
+      state.mapUrl = payload;
+    },
+    setBgUrl(state, payload) {
+      state.bgUrl = payload;
+    },
+  },
   actions: {},
   modules: {},
 });
