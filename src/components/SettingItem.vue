90acfc8178f133f454a7b2c977b5a79a8dbcd8c2
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-16 20:25:21 +0800

    feat: Added settingitem component

---
+++ src/components/SettingItem.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  .setting-item
+    h1.title {{ title }}
+    p.description {{ description }}
+    .input-wrap
+      input(
+        :placeholder="placeholder" 
+        :value="initialData" 
+        @change="emitInput"
+      )
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    initialData: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    emitInput(evt: Event): void {
+      this.$emit("change", (evt.target as HTMLInputElement).value);
+    },
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.setting-item {
+  margin-bottom: 20px;
+  .input-wrap {
+    margin-top: 10px;
+    input {
+      outline: none;
+      border: none;
+      border-radius: 5px;
+      width: 300px;
+      padding: 10px;
+    }
+  }
+}
+</style>
