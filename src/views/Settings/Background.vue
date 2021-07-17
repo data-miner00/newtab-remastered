4037682fe07200fe621b5a446e855480370977f8
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-17 20:16:09 +0800

    feat: Background changer

---
+++ src/views/Settings/Background.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  .background
+    SettingItem(
+      title="Set background image"
+      description="Now, you are able to cuztomize the background picture however you want!!! Just provide the correct URL to the image will do."
+      placeholder="eg. /bg.jpg"
+      :initialData="initialBg"
+      @change="bgChange"
+    )
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import SettingItem from "@/components/SettingItem.vue";
+import { setLS } from "@/services";
+import { StorageType } from "@/models/StorageType";
+
+export default Vue.extend({
+  components: {
+    SettingItem,
+  },
+  methods: {
+    bgChange(newUrl: string): void {
+      setLS(StorageType.BGURL, newUrl);
+    },
+  },
+  computed: {
+    initialBg(): string {
+      return this.$store.state.bgUrl || "";
+    },
+  },
+});
+</script>
+
+<style lang="less" scoped>
+//
+</style>
