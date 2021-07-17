a2d51b53789985c37856a341d8a0f6321890db2a
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-18 03:17:39 +0800

    feat: Server in settings

---
+++ src/views/Settings/Server.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  .server
+    h1.title Server settings
+    .description The server and its functionalities are currently unavailable.
+</template>
+
+<style lang="less" scoped></style>
