eba4de95ca43715fc8d1aa95722015ba900a2418
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-17 21:44:10 +0800

    feat: Added switch component

---
+++ src/components/SettingItemSwitch.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  .setting-item-switch
+    h1.title {{ title }}
+    p.description {{ description }}
+    label.switch
+      input(type="checkbox" @change="onValueChange" :checked="initialData")
+      span.slider.round
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    initialData: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    onValueChange(evt: Event): void {
+      this.$emit("change", (evt.target as HTMLInputElement).checked);
+    },
+  },
+});
+</script>
+
+<style lang="less" scoped>
+@import "../assets/less/components/switch.less";
+
+.setting-item-switch {
+  margin-bottom: 20px;
+
+  .description {
+    margin-bottom: 10px;
+  }
+}
+</style>
