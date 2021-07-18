988d855375b2f09be619775fc71358abb062baee
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-18 12:20:08 +0800

    refactor: Externalize storage worker

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,53 +1,7 @@
 import store from "@/store";
 import { StorageType } from "@/models/StorageType";
 
-export const loadLS = (type: StorageType): void => {
-  const value: string | null = localStorage.getItem(type);
-
-  if (value) {
-    // TODO: Externalize switch case to a seperate func
-    switch (type) {
-      case StorageType.USERNAME:
-        store.commit("setUsername", value);
-        break;
-      case StorageType.AVATARURL:
-        store.commit("setAvatarUrl", value);
-        break;
-      case StorageType.MAILURL:
-        store.commit("setMailUrl", value);
-        break;
-      case StorageType.NEWSURL:
-        store.commit("setNewsUrl", value);
-        break;
-      case StorageType.MAPURL:
-        store.commit("setMapUrl", value);
-        break;
-      case StorageType.BGURL:
-        store.commit("setBgUrl", value);
-        break;
-      case StorageType.CLOCKMODE:
-        store.commit("setClockMode", JSON.parse(value));
-        break;
-      case StorageType.COLONMODE:
-        store.commit("setColonMode", JSON.parse(value));
-        break;
-      case StorageType.PLACE:
-        store.commit("setPlace", value);
-        break;
-      default:
-        console.log("u wot m8");
-    }
-  } else
-    console.log(
-      `%c Notice: %c The value for ${type} does not exist`,
-      "color: white; background: #00d1b2; font-weight: bold; border-radius: 2px; ",
-      "color: #00947e; background: #ebfffc;"
-    );
-};
-
-export const setLS = (type: StorageType, value: string): void => {
-  localStorage.setItem(type, value);
-
+const storageWorker = (type: StorageType, value: string) => {
   switch (type) {
     case StorageType.USERNAME:
       store.commit("setUsername", value);
@@ -79,4 +33,24 @@ export const setLS = (type: StorageType, value: string): void => {
     default:
       console.log("u wot m8");
   }
+};
+
+export const loadLS = (type: StorageType): void => {
+  const value: string | null = localStorage.getItem(type);
+
+  if (value) {
+    // TODO: Externalize switch case to a seperate func
+    storageWorker(type, value);
+  } else
+    console.log(
+      `%c Notice: %c The value for ${type} does not exist`,
+      "color: white; background: #00d1b2; font-weight: bold; border-radius: 2px; ",
+      "color: #00947e; background: #ebfffc;"
+    );
+};
+
+export const setLS = (type: StorageType, value: string): void => {
+  localStorage.setItem(type, value);
+
+  storageWorker(type, value);
 };
