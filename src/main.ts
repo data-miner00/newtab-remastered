81d333a7646e67097246203ca434dbcf7ad04ace
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-18 11:45:42 +0800

    feat: Registered vue-meta

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueMeta from "vue-meta";
 import "./plugins/fontAwesome";
+
+Vue.use(VueMeta);
 
 Vue.config.productionTip = false;
 
