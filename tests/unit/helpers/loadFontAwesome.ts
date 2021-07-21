fad185b9da3cb8dd35898199cd7d3e1f16e33148
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-21 10:41:11 +0800

    test: Added font-awesome helper

---
+++ tests/unit/helpers/loadFontAwesome.ts
@@ -0,0 +1,49 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faCoffee,
+  faChevronDown,
+  faCaretDown,
+  // faCloud,
+  // faCloudMeatball,
+  // faCloudMoon,
+  // faCloudMoonRain,
+  // faCloudRain,
+  // faCloudShowersHeavy,
+  // faCloudSun,
+  // faCloudSunRain,
+  // faMapMarkerAlt,
+  faArrowLeft,
+  faLanguage,
+  faHandSparkles,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faComment as farComment,
+  faEnvelope as farEnvelope,
+  faNewspaper as farNewspaper,
+  faObjectUngroup as farObjectUngroup,
+  faMap as farMap,
+} from "@fortawesome/free-regular-svg-icons";
+
+library.add(
+  faCoffee,
+  farComment,
+  farNewspaper,
+  farEnvelope,
+  faChevronDown,
+  faCaretDown,
+  farObjectUngroup,
+  farMap, // not used
+  // faCloud, // not used
+  // faCloudMeatball, // not used
+  // faCloudMoon, // not used
+  // faCloudMoonRain, // not used
+  // faCloudRain, // not used
+  // faCloudShowersHeavy, // not used
+  // faCloudSun, // not used
+  // faCloudSunRain, // not used
+  // faMapMarkerAlt, // not used
+  faArrowLeft,
+  faLanguage,
+  faHandSparkles
+);
