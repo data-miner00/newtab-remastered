933f16840bf3a5cabac61f23b8eebcc41ffe007c
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-16 17:58:09 +0800

    🚧 Stage misc file

---
+++ src/views/Settings/Misc.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    .misc Language
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  //
+});
+</script>
+
+<style lang="less" scoped></style>
