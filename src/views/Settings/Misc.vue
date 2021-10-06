94ed55b22f0cd4e8a40d244b65e7777f9e6c06f2
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-06 19:13:59 +0800

    feat: Language select UI

--- src/views/Settings/Misc.vue
+++ src/views/Settings/Misc.vue
@@ -1,11 +1,28 @@
 <template lang="pug">
-    .misc Language
+    .misc
+      SettingDropdownItem(
+        title="Language"
+        description="The display language that you prefer, however some part of the app may not be reflected as expected."
+        :items="languages"
+        subject="language"
+        @change="languageChange"
+      )
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import SettingDropdownItem from "@/components/SettingDropdownItem.vue";
+
 export default Vue.extend({
-  //
+  components: { SettingDropdownItem },
+  data: () => ({
+    languages: ["English", "Japanese"],
+  }),
+  methods: {
+    languageChange(language: string): void {
+      console.log(language);
+    },
+  },
 });
 </script>
 
